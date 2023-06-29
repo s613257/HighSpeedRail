aecb2a9932b326a170a236852d1ebee84caf2703
明敏 <s613257@gmail.com>
2023-06-29 18:39:28 +0800

    0629 增加了批次上傳

--- TravelMaster/src/main/java/com/tm/TravelMaster/TravelMasterApplication.java
+++ TravelMaster/src/main/java/com/tm/TravelMaster/TravelMasterApplication.java
@@ -3,7 +3,7 @@ package com.tm.TravelMaster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class TravelMasterApplication {
 
 	public static void main(String[] args) {
